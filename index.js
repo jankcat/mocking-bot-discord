e62e263f396257071455b5972c4242fec2b612cd
Tim Watkins <timothy.watkins@gmail.com>
2019-03-31 20:55:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -60,10 +60,23 @@ client.on('messageReactionAdd', async (reaction, user) => {
   
   if (reaction.emoji.name === 'mocking') await mockingReaction(reaction, user);
   if (reaction.emoji.name === 'shrek') await shrekReaction(reaction, user);
+  if (reaction.emoji.name === 'shrek2') await shrek2Reaction(reaction, user);
 });
 
+const shrek2Reaction = async function(reaction, user) {
+  // React with shrek2 so the same user can't keep unreacting and reacting
+  await reaction.message.react(reaction.emoji);
+  
+  await reaction.message.channel.send("Somebody once told me the world is gonna roll me");
+  await reaction.message.channel.send("I ain't the sharpest tool in the shed");
+  await reaction.message.channel.send("She was looking kind of dumb with her finger and her thumb");
+  await reaction.message.channel.send("In the shape of an \"L\" on her forehead");
+  
+  console.log(`[${reaction.message.guild.name}][${reaction.message.channel.name}] ${user.username} thinks ${reaction.message.author.username} is an all-star and needs to get their game on`);
+};
+
 const shrekReaction = async function(reaction, user) {
-  // React with mocking so the same user can't keep unreacting and reacting
+  // React with shrek so the same user can't keep unreacting and reacting
   await reaction.message.react(reaction.emoji);
   
   await reaction.message.react('🇭');
