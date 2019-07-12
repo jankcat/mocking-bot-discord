6e592bd5f0016baebd98eab287b1a89ab909ac48
partywhale <jameserson@gmail.com>
2019-07-12 13:53:22 -0700

    Add Darth Plagueis reaction

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const shrekReaction = require('./commands/shrekReaction');
 const mockingReaction = require('./commands/mockingReaction');
 const prequelMemes = require('./commands/prequelMemes');
 const prequelTts = require('./commands/prequelTts');
+const darthPlagueis = require('./commands/darthPlagueis');
 const guilty = require('./commands/guilty');
 
 const client = new Discord.Client({
@@ -43,6 +44,8 @@ client.on('message', async (message) => {
     await prequelTts(message);
   } else if (message.content.toLowerCase() === '/guilty') {
     await guilty(message);
+  } else if (message.content.toLowerCase() === 'did you ever hear the tragedy of darth plagueis the wise?') {
+    await darthPlagueis(message);
   }
 });
 
