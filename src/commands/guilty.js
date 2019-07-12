e4f3953abb1d4dd7db07ffe0c8e98d928f9bbb1c
Tim Watkins <timothy.watkins@gmail.com>
2019-07-12 10:51:46 -0600

    Update guilty.js

--- src/commands/guilty.js
+++ src/commands/guilty.js
@@ -1,3 +1,5 @@
+const { Attachment } = require('discord.js');
+
 module.exports = async function(message, term) {
   const image = new Attachment('https://raw.githubusercontent.com/jankcat/mocking-bot-discord/master/guilty.gif');
   message.channel.send(image);
