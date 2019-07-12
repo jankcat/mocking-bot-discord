da7c960d837be5c968409c0bdea9987354ac57c4
Tim Watkins <timothy.watkins@gmail.com>
2019-07-12 11:06:14 -0600

    Update guilty.js

--- src/commands/guilty.js
+++ src/commands/guilty.js
@@ -1,6 +1,15 @@
 const { Attachment } = require('discord.js');
 
 module.exports = async function(message, term) {
-  const image = new Attachment('https://raw.githubusercontent.com/jankcat/mocking-bot-discord/master/guilty.gif');
+  var gif = gifs[Math.floor(Math.random()*gifs.length)];
+  const image = new Attachment(gif);
   message.channel.send(image);
 };
+
+const gifs = [
+  'https://raw.githubusercontent.com/jankcat/mocking-bot-discord/master/guilty.gif',
+  'https://raw.githubusercontent.com/jankcat/mocking-bot-discord/master/guilty2.gif',
+  'https://raw.githubusercontent.com/jankcat/mocking-bot-discord/master/guilty3.gif',
+  'https://raw.githubusercontent.com/jankcat/mocking-bot-discord/master/guilty4.gif',
+  'https://raw.githubusercontent.com/jankcat/mocking-bot-discord/master/guilty5.gif',
+];
