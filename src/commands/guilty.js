8b47a4a9461208625b85b538edd5cfc66b7a14c1
Tim Watkins <timothy.watkins@gmail.com>
2019-07-12 11:20:18 -0600

    Update guilty.js

--- src/commands/guilty.js
+++ src/commands/guilty.js
@@ -4,6 +4,7 @@ module.exports = async function(message, term) {
   var gif = gifs[Math.floor(Math.random()*gifs.length)];
   const image = new Attachment(gif);
   message.channel.send(image);
+  console.log(`[${message.guild.name}][${message.channel.name}] ${message.author.username} is guilty and sentenced to the electric shaver.`);
 };
 
 const gifs = [
