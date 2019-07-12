1d45e6fb53f044a857313757d0e7074f2a9ef025
Tim Watkins <timothy.watkins@gmail.com>
2019-07-12 11:19:50 -0600

    Update prequelTts.js

--- src/commands/prequelTts.js
+++ src/commands/prequelTts.js
@@ -5,4 +5,5 @@ module.exports = async function(message) {
       tts: true,
     }
   );
+  console.log(`[${message.guild.name}][${message.channel.name}] ${message.author.username} is a bold one.`);
 };
