8b981f1b9cbf4484e2581086d28f3c744c3fa73f
Tim Watkins <timothy.watkins@gmail.com>
2019-07-17 16:43:49 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const client = new Discord.Client({
 client.on('error', console.error);
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.guilds.tap(guild => console.log(`[${guild.name}] [ready] Rejoining server.`));
 });
 
 client.on('guildCreate', async (guild) => {
